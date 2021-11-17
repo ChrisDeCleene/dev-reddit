507503ba0799bfd97346c2bed7ba7d16a63e07db
Chris DeCleene <chris_decleene@ymail.com>
2021-11-16 21:03:07 -0700

    Import posts selectors, actions and dispatch. Remove and replace placeholder post data with API posts stored in Redux state.

--- src/components/Layout/PostsPage/PostsPage.js
+++ src/components/Layout/PostsPage/PostsPage.js
@@ -1,41 +1,20 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useEffect } from "react";
 import classes from "./PostsPage.module.css";
 import Post from "../Post/Post";
 import LoadingPosts from "../LoadingPosts/LoadingPost";
-
-const post = {
-  author: "michael0x2a",
-  author_fullname: "t2_aoqnb",
-  created: 1490569654,
-  created_utc: 1490569654,
-  id: "61oly8",
-  name: "t3_61oly8",
-  num_comments: 14,
-  num_crossposts: 8,
-  permalink: "/r/learnprogramming/comments/61oly8/new_read_me_first/",
-  score: 828,
-  selftext:
-    "How do guys who say they program 8 hours a day manage to sit there all day? I feel like doing something else after 1 hour of being on even when doing something I somewhat enjoy like gaming... I don't know if it is because I have low attention span or uncomfortable cheap office chair and desk or maybe a combo of both.",
-  title: "How to sit at PC for long periods of time?",
-  ups: 828,
-  upvote_ratio: 0.98,
-  url: "https://www.reddit.com/r/learnprogramming/comments/61oly8/new_read_me_first/",
-};
-
-const posts = [post, post, post, post, post, post];
+import { useDispatch, useSelector } from "react-redux";
+import { currentSubredditSelector, fetchPosts, loadingSelector, postsSelector } from "../../../slice/postsSlice";
 
 const PostsPage = () => {
-  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  const subreddit = useSelector(currentSubredditSelector);
+  const loading = useSelector(loadingSelector);
+  const posts = useSelector(postsSelector);
 
-  // REMOVE THIS ONCE API CONNECTED
   useEffect(() => {
-    const updateLoading = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-    return () => {
-      clearTimeout(updateLoading);
-    };
-  }, []);
+    dispatch(fetchPosts(subreddit))
+  }, [dispatch, subreddit]);
+
   return (
     <div className={classes.posts}>
       <h1>LearnProgramming</h1>
@@ -47,7 +26,7 @@ const PostsPage = () => {
           <LoadingPosts />
         </Fragment>
       ) : (
-        posts.map((post) => <Post key={post.id + Math.random()} post={post} />)
+        posts.map((post) => <Post key={post.id} post={post} />)
       )}
     </div>
   );
