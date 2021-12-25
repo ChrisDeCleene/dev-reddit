bbb893a27b901302d3da416d3292f92dc259580f
Chris DeCleene <chris_decleene@ymail.com>
2021-12-24 17:00:43 -0700

    Import subredditTitleSelector and render in Component h1.

--- src/components/Layout/PostsPage/PostsPage.js
+++ src/components/Layout/PostsPage/PostsPage.js
@@ -3,21 +3,28 @@ import classes from "./PostsPage.module.css";
 import Post from "../Post/Post";
 import LoadingPosts from "../LoadingPosts/LoadingPost";
 import { useDispatch, useSelector } from "react-redux";
-import { currentSubredditSelector, fetchPosts, loadingSelector, postsSelector } from "../../../slice/postsSlice";
+import {
+  currentSubredditSelector,
+  fetchPosts,
+  loadingSelector,
+  postsSelector,
+  subredditTitleSelector,
+} from "../../../slice/postsSlice";
 
 const PostsPage = () => {
   const dispatch = useDispatch();
   const subreddit = useSelector(currentSubredditSelector);
   const loading = useSelector(loadingSelector);
   const posts = useSelector(postsSelector);
+  const subredditTitle = useSelector(subredditTitleSelector);
 
   useEffect(() => {
-    dispatch(fetchPosts(subreddit))
+    dispatch(fetchPosts(subreddit));
   }, [dispatch, subreddit]);
 
   return (
     <div className={classes.posts}>
-      <h1>LearnProgramming</h1>
+      <h1>{subredditTitle}</h1>
       {loading && <p>Loading posts, please wait...</p>}
       {loading ? (
         <Fragment>
