f68b926433e3123e21062139dcf6310116a2446e
Chris DeCleene <chris_decleene@ymail.com>
2022-01-03 15:47:02 -0700

    Remove logs from development stage

--- src/api/reddit.js
+++ src/api/reddit.js
@@ -3,12 +3,10 @@ export const API_ROOT = "https://www.reddit.com";
 export const getSubredditPosts = async (subreddit) => {
   const response = await fetch(`${API_ROOT}/r/${subreddit}.json`);
   const json = await response.json();
-
   return json.data.children.map((post) => post.data);
 };
 
 export const getPostComments = async (permalink) => {
-  console.log(`${API_ROOT}${permalink}.json`);
   const response = await fetch(`${API_ROOT}${permalink}.json`);
   const json = await response.json();
   return json[1].data.children.map((subreddit) => subreddit.data);
