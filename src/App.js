ef2d7917f3d42f4868ffa1a1c2d858a2437503d8
Chris DeCleene <chris_decleene@ymail.com>
2022-01-16 13:00:00 -0700

    Set up react router routes for application

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React, { Fragment } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Layout/Header/Header";
 import PostsPage from "./pages/PostsPage/";
-import classes from "./App.module.css";
+import PostDetail from "./pages/PostDetail";
+import styles from "./App.module.css";
 
 const App = () => {
   return (
-    <Fragment>
+    <Router>
       <Header />
-      <main className={classes.main}>
-        <PostsPage />
+      <main className={styles.main}>
+        <Routes>
+          <Route path="/" element={<PostsPage />} />
+          <Route path="/:postId" element={<PostDetail />} />
+        </Routes>
       </main>
-    </Fragment>
-  )
-}
+    </Router>
+  );
+};
 
 export default App;
